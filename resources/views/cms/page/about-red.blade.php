b77884b960c5d527e66aa2bf90ba4731d00e94bb
lifosmin <65651883+lifosmin@users.noreply.github.com>
2021-04-02 00:48:22 +0700

    some changes

--- resources/views/cms/page/about-red.blade.php
+++ resources/views/cms/page/about-red.blade.php
@@ -7,13 +7,13 @@
 @section('content')
 <div class="main">
     <img src="{{ asset('images/about-red/cloud.png') }}" class="cloud">
-    <img src="{{ asset('images/about-red/circle.png') }}" class="circle" > 
+    
     
     
     <div class="container section-1">
         <div class="row">
-            <div class="col-6 red-content"> <h1>hello</h1> </div>
-            <div class="col-6 red-content"> 
+            <div class="col-lg-6 col-sm-12 red-content logo"> <img src="{{ asset('images/about-red/logo-red.png') }}" class="logo-red"></div>
+            <div class="col-lg-6 col-sm-12 red-content text"> 
                 <h1>WHAT IS RED?</h1> 
                 <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
             </div>
@@ -24,6 +24,7 @@
         <img src="{{ asset('images/about-red/city.png') }}" class="city"> 
 
     </div>
+    <img src="{{ asset('images/about-red/circle.png') }}" class="circle d-block d-lg-block d-sm-none" > 
 </div>
 
 
