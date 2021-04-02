98b19beaaa5745fc77e9f775d0aae1ba3676cb07
lifosmin <65651883+lifosmin@users.noreply.github.com>
2021-04-02 14:55:21 +0700

    add logo, start work on js modal, some changes

---
+++ public/js/cms/page/about-red.js
@@ -0,0 +1,26 @@
+// var riseBtn = document.getElementById("rise-btn");
+// var raconteurBtn = document.getElementById("raconteur-btn");
+// var utopiaBtn = document.getElementById("utopia-btn");
+// var utileBtn = document.getElementById("utile-btn");
+
+// var rise = document.getElementById("rise");
+// var raconteur = document.getElementById("raconteur");
+// var utopia = document.getElementById("utopia");
+// var utile = document.getElementById("utile");
+
+// riseBtn.onclick = function(){
+//     rise.style.display = "block";
+// }
+// raconteurBtn.onclick = function(){
+//     raconteur.style.display = "block";
+// }
+// utopiaBtn.onclick = function(){
+//     utopia.style.display = "block";
+// }
+// utileBtn.onclick = function(){
+//     utile.style.display = "block";
+// }
+
+function togglePopup(){
+    document.getElementById("rise").classList.toggle("active");
+}
--- resources/views/cms/page/about-red.blade.php
+++ resources/views/cms/page/about-red.blade.php
@@ -7,12 +7,12 @@
 @section('content')
 <div class="main">
     <img src="{{ asset('images/about-red/cloud.png') }}" class="cloud">
-    
+    <img src="{{ asset('images/about-red/circle.png') }}" class="circle d-block d-lg-block d-sm-none" > 
     
     
     <div class="container section-1">
         <div class="row">
-            <div class="col-lg-6 col-sm-12 red-content logo"> <img src="{{ asset('images/about-red/logo-red.png') }}" class="logo-red"></div>
+            <div class="col-lg-6 col-sm-12 red-content about-logo"> <img src="{{ asset('images/about-red/logo-red.png') }}" class="logo-red"></div>
             <div class="col-lg-6 col-sm-12 red-content text"> 
                 <h1>WHAT IS RED?</h1> 
                 <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
@@ -20,12 +20,55 @@
             
         </div>
     </div>
+
     <div class="section-2">
         <img src="{{ asset('images/about-red/city.png') }}" class="city"> 
-
     </div>
-    <img src="{{ asset('images/about-red/circle.png') }}" class="circle d-block d-lg-block d-sm-none" > 
+    
+    <div class="section-3 ">
+        <div class="row mission-logo-container-row container">
+            <div class="col-lg-6 col-sm-12 mission-logo-container-col"> <span class="mission-logo" onclick="togglePopup()"id="rise-btn"></span> </div>
+            <div class="col-lg-6 col-sm-12 mission-logo-container-col"> <span class="mission-logo" id="utopia-btn"></span>  </div>
+            <div class="col-lg-6 col-sm-12 mission-logo-container-col"> <span class="mission-logo" id="raconteur-btn"></span>  </div>
+            <div class="col-lg-6 col-sm-12 mission-logo-container-col"> <span class="mission-logo" id="utile-btn"></span>  </div>
+
+            
+            <!-- <div class="col-lg-6 col-sm-12 "> <img src="{{ asset('images/about-red/temp-logo.png') }}" class=""></div>
+            <div class="col-lg-6 col-sm-12 "> <img src="{{ asset('images/about-red/temp-logo.png') }}" class=""></div>
+            <div class="col-lg-6 col-sm-12 "> <img src="{{ asset('images/about-red/temp-logo.png') }}" class=""></div>
+            <div class="col-lg-6 col-sm-12 "> <img src="{{ asset('images/about-red/temp-logo.png') }}" class=""></div> -->
+        </div>
+        <div class="popup-page" id="rise">
+                <div class="popup-content">
+                    <span class="close-btn">&times;</span>
+                    <div class="row mission-content">
+                        <div class="col-6">
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+
+                        </div>
+                        <div class="col-6">
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+
+                        </div>
+                    </div>
+                </div>
+        </div>
+    </div>
+
+    <div class="section-4 row">
+        <a href="#" class="button-container">
+            <img src="{{ asset('images/about-red/button.png') }}" class="button"> 
+            <p class="button-text"> BECOME ECOFRIENDS</p>
+        </a>
+        
+    </div>
 </div>
 
+
+@endsection
+
+@section('custom-js')
+
+<script src="{{ asset('js/cms/page/about-red.js') }}"></script>
 
 @endsection
