4f7ca2f2380ed4fd6c9bd590285d09f72c05332c
kevkrisss <kevin.jonathan5@student.umn.ac.id>
2021-04-02 14:24:44 +0700

    Header complete

--- resources/views/cms/page/profile.blade.php
+++ resources/views/cms/page/profile.blade.php
@@ -16,6 +16,9 @@
         <div class="profile-name">
             NIKOLAS
         </div>
+        <a href="" class="logout">
+            <img src="images/profile/Log Out@2x.png" alt=""><p>LOG OUT</p>
+        </a>
         </div>
         <div class="profile-data">
             ECO Friend Profile
@@ -31,5 +34,14 @@
                 Student Email:
             </div>
         </div>
+<!--         
+        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="/home">Logout<i class="fa fa-power-off"></i></a></li> -->
+        
+        <div class="profile-journey-bg" style="padding-top:10rem;padding-bottom:10rem;background-image:url({{ asset('images/profile/Bar Merah Your Journey@2x.png') }});">
+            <div class="profile-journey">
+                YOUR JOURNEY IN RED
+            </div>
+        </div>
     </div>
+</div>
 @endsection
