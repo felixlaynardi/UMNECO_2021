22bd372320e332360c1a361cdc20510339773f1b
chotiwood <chotiwut11@gmail.com>
2021-04-03 23:10:57 +0700

    Update Login dan Register

--- resources/views/cms/page/register.blade.php
+++ resources/views/cms/page/register.blade.php
@@ -1,72 +1,99 @@
 <!--Ganti error message didalem div yang classnya error, if nya sesuain cari error gara2 apa -->
+@extends('cms.template.app')
 
-<div class="container-fluid">
-    <img src="/images/regis/Header.png">
-</div>
-<form action="{{route('register')}}" method="post" class="form">
+@section('custom-css')
+    <link rel="stylesheet" href="{{ asset('css/cms/page/regis.css') }}">
+@endsection
+
+@section('content')
+<header>
+<h5>Real Emergency Daily life</h5>
+<h1>REGISTRATION</h1>
+</header>
+<div class="form">
+<form action="{{route('register')}}" method="post" class="form-">
     @csrf
-    <input type="text" name="name" id="name" placeholder="name">
+    <label>Nama Lengkap</label>
+    <input type="text" name="name" id="name" placeholder="UMN ECO" class="form-control"><br>
     @if($errors->has('name'))
     <div class="error">{{ $errors->first('name') }}</div>
     @endif
-    <input type="text" name="student_id" id="student_id" placeholder="NIM">
+    <label>NIM</label>
+    <input type="text" name="student_id" id="student_id" placeholder="0000001234" class="form-control"><br>
     @if($errors->has('student_id'))
     <div class="error">{{ $errors->first('student_id') }}</div>
     @endif
-    <input type="text" name="email" id="email" placeholder="email">
+    <label>Email</label>
+    <input type="text" name="email" id="email" placeholder="eco@umn.ac.id" class="form-control"><br>
     @if($errors->has('email'))
     <div class="error">{{ $errors->first('email') }}</div>
     @endif
-    <select id="major" name="major">
+    <label>jurusan</label>
+    <select id="major" name="major" class="form-control"><br>
         <option value="Informatika">Informatika</option>
         <option value="Film & Animasi">Film & Animasi</option>
         <option value="Strategic Communication">Strategic Communication</option>
         <option value="Akuntansi">Akuntansi</option>
-    </select>
+    </select><br>
     @if($errors->has('major'))
     <div class="error">{{ $errors->first('major') }}</div>
     @endif
-    <select id="generation" name="generation">
+    <label>Angkatan</label>
+    <select id="generation" name="generation" class="form-control">
         <option value="2016">2016</option>
         <option value="2017">2017</option>
         <option value="2018">2018</option>
         <option value="2019">2019</option>
         <option value="2020">2020</option>
-    </select>
+    </select><br>
     @if($errors->has('generation'))
     <div class="error">{{ $errors->first('generation') }}</div>
     @endif
-    <input type="text" name="instagram_account" id="instagram_account" placeholder="instagram_account">
+    <label>Username IG (tidak di-private)</label>
+    <input type="text" name="instagram_account" id="instagram_account" placeholder="umn_eco" class="form-control"><br>
     @if($errors->has('instagram_account'))
     <div class="error">{{ $errors->first('instagram_account') }}</div>
     @endif
-    <input type="text" name="line_id" id="line_id" placeholder="line_id">
+    <label>ID Line</label>
+    <input type="text" name="line_id" id="line_id" placeholder="line_id" class="form-control"><br>
     @if($errors->has('line_id'))
     <div class="error">{{ $errors->first('line_id') }}</div>
     @endif
-    <input type="text" name="phone_number" id="phone_number" placeholder="phone_number">
+    <label>Nomor Telepon</label>
+    <input type="text" name="phone_number" id="phone_number" placeholder="081235678910" class="form-control"><Br>
     @if($errors->has('phone_number'))
     <div class="error">{{ $errors->first('phone_number') }}</div>
     @endif
-    <input type="password" name="password" id="password" placeholder="password">
+    <label>Password</label>
+    <input type="password" name="password" id="password" placeholder="password" class="form-control"><br>
     @if($errors->has('password'))
     <div class="error">{{ $errors->first('password') }}</div>
     @endif
-    <input type="password" name="confirm_password" id="confirm_password" placeholder="confirm_password">
+    <label>Konfirmasi Password</label>
+    <input type="password" name="confirm_password" id="confirm password" placeholder="confirm_password" class="form-control">
     @if($errors->has('confirm_password'))
     <div class="error">{{ $errors->first('confirm_password') }}</div>
     @endif
     <input type="checkbox" id="availability" name="availability" value="1">
-    <label for="availability"> Bersedia menjalankan kegiatan Real Emergency Dailylife selama 12 hari </label><br>
+    <label for="availability" class="checkbox"> Bersedia menjalankan kegiatan Real Emergency Dailylife selama 12 hari </label><br>
     @if($errors->has('availability'))
     <div class="error">{{ $errors->first('availability') }}</div>
     @endif
 
-
+    <br>
     <button type="submit" class="btn btn-primary">Submit</button>
 </form>
+</div>
 <footer>
-<img src="/images/login/Footer.png">
-<img src="/images/login/Logo Footer.png">
+<img src="{{ asset('images/regis/Logo Footer.png')}}" class="logo">
 </footer>
 </header>
+
+@endsection
+
+@section('custom-js')
+    <script src="{{ asset('js/cms/page/home.js') }}"></script>
+    <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/bodymovin/5.7.6/lottie.min.js"></script> -->
+
+    <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+@endsection
