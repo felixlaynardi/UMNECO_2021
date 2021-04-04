b9a66ca6aec0f6665d6d6c3cb7fdfc70acae246b
kevkrisss <kevin.jonathan5@student.umn.ac.id>
2021-04-05 00:37:32 +0700

    Complete profile responsive (unfinished progress bar)

--- resources/views/cms/page/profile.blade.php
+++ resources/views/cms/page/profile.blade.php
@@ -7,9 +7,8 @@
 <link rel="stylesheet" href="{{ asset('css/cms/page/profile.css') }}">
 @endsection
 
-@section('content')
+@section('content') 
 <div class="main-container pt-5">
-    
     <div class="row-text-center">      
         <div class="profile-header" style="padding-top:10rem;padding-bottom:10rem;background-image:url({{ asset('images/profile/Header@2x.png') }});">        
             <div class="container">    
@@ -27,46 +26,49 @@
         </div>
     </div>
     
-    <div class="container">
-        <div class="row text-center"id ="data">    
-            <div class="profile-data">
-                ECO Friend Profile
-            </div>
-        <div>
-    </div>
-    <div class="container">
-        <div class="row text-center"id ="name">
-            <div class="profile-name-eco-friend col-sm-12 col-md-12 col-lg-3">
-                <p style="text-transform: capitalize;">Name </p>
-            </div>
-            <div class="profile-name-eco-friend col-sm-12 col-md-12 col-lg-5">
-                <p style="text-transform: capitalize;"> : {{$data->name}}</p>
+    <div class="container" id="eco-friend-identity">
+        <div class="container">
+            <div class="row text-center"id ="data">    
+                <div class="profile-data">
+                    ECO Friend Profile
+                </div>
             </div>
         </div>
+        
+        <div class="container">
+            <div class="row text-center"id ="name">
+                <div class="profile-name-eco-friend col-sm-12 col-md-12 col-lg-3">
+                    <p style="text-transform: capitalize;">Name </p>
+                </div>
+                <div class="profile-name-eco-friend col-sm-12 col-md-12 col-lg-5">
+                    <p style="text-transform: capitalize;"> : {{$data->name}}</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="container">
+            <div class="row text-center"id ="nim">
+                <div class="profile-nim-eco-friend col-sm-12 col-md-12 col-lg-3">
+                    <p>NIM</p>
+                </div>
+                <div class="profile-nim-eco-friend col-sm-12 col-md-12 col-lg-5">
+                    <p>: {{$data->student_id}}</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="container">
+            <div class="row text-center"id ="email">
+                <div class="profile-email-eco-friend col-sm-12 col-md-12 col-lg-3">
+                    <p>Student Email </p>
+                </div>  
+                <div class="profile-email-eco-friend col-sm-12 col-md-12 col-lg-9">
+                    <p>: {{$data->email}}</p>
+                </div>  
+            </div>
+        </div>  
     </div>
 
-    <div class="container">
-        <div class="row text-center"id ="nim">
-            <div class="profile-nim-eco-friend col-sm-12 col-md-12 col-lg-3">
-                <p>NIM</p>
-            </div>
-            <div class="profile-nim-eco-friend col-sm-12 col-md-12 col-lg-5">
-                <p>: {{$data->student_id}}</p>
-            </div>
-        </div>
-    </div>
-
-    <div class="container">
-        <div class="row text-center"id ="email">
-            <div class="profile-email-eco-friend col-sm-12 col-md-12 col-lg-3">
-                <p>Student Email </p>
-            </div>  
-            <div class="profile-email-eco-friend col-sm-12 col-md-12 col-lg-9">
-                <p>: {{$data->email}}</p>
-            </div>  
-        </div>
-    </div>  
-    
     <div class="container">
         <div class="row-text-center" >
             <div class="profile-journey">
@@ -87,11 +89,11 @@
             </div>
         </div>
     </div>
-    <div class="container">
+    
     <div class="container progress-graph">
         <div class="chart small-font-size">
             <!-- bar pertama -->
-            <div class="bar bar-60 utopia">
+            <div class="bar bar-60 utopia col-sm-3">
                 <img src="{{ asset('images/profile/Utopia Temp Logo@2x.png') }}" alt="">
                 <div class="face side-0">
                     <div class="growing-bar"></div>
@@ -103,7 +105,7 @@
                 <div class="face floor"></div>
             </div>
             <!-- bar kedua -->
-            <div class="bar bar-25 rise">
+            <div class="bar bar-25 rise col-sm-3">
                 <img style="width: 2em" src="{{ asset('images/profile/Rise Temp Logo@2x.png') }}" alt="">
                 <div class="face side-0">
                     <div class="growing-bar"></div>
@@ -115,7 +117,7 @@
                 <div class="face floor"></div>
             </div>
             <!-- bar ketiga -->
-            <div class="bar bar-35 utile">
+            <div class="bar bar-35 utile col-sm-3">
                 <img style="width: 2em" src="{{ asset('images/profile/Mystery@2x.png') }}" alt="">
                 <div class="face side-0">
                     <div class="growing-bar"></div>
@@ -127,7 +129,7 @@
                 <div class="face floor"></div>
             </div>
             <!-- bar keempat -->
-            <div class="bar bar-80 racounter">
+            <div class="bar bar-80 racounter col-sm-3">
                 <img style="width: 2em" src="{{ asset('images/profile/Mystery@2x.png') }}" alt="">
                 <div class="face side-0">
                     <div class="growing-bar"></div>
@@ -140,23 +142,51 @@
             </div>
         </div>
     </div>
+    
+    <div class="container" id="button-submit">
+        <div class="row">
+            <div class="col-3">
+                <div class="logo-submit">
+                    <button class="submit-button" id="submit-utopia">Submit</button>
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="logo-submit">
+                    <button class="submit-button"id="submit-rise">Submit</button>
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="logo-submit">
+                    <button class="submit-button"id="submit-utile">Submit</button>
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="logo-submit">
+                    <button class="submit-button"id="submit-racounter">Submit</button>
+                </div>
+            </div>
+        </div>
     </div>
 
-    <div class="logo row mx-auto">
-        <div class="logo-submit">
-            <button class="submit">Submit</button>
+    <div class="col-12" id="footer">        
+        <div class="profile-footer" style="padding-top:10rem;padding-bottom:10rem;background-image: linear-gradient(to top, #d74646, #e15251, #eb5d5d, #f56968, #ff7474);;">   
+            <div class="container" id="footer-items">      
+                    <div class="eco-umn-tagline">
+                        <p>#WeActForTheBetterEarth</p>
+                    </div>
+                    <div class="eco-red">
+                        <p>#RealEmergencyDailyLife</p>
+                    </div>
+                    <div class="logo-umn-eco">
+                        <img src="/images/profile/Logo UMN@2x.png" alt="logo-umn-eco">
+                    </div>
+                </div>
+            </div>
         </div>
-        <!-- <div class="logo-submit">
-            <a href="#1" class="open-popup"><img src="{{ asset('images/oprec/logo/GANTAR.png') }}" alt="" class="image-fluid" onclick="mBlur();"></a>
-        </div>
-        <div class="logo-submit">
-            <a href="#2" class="open-popup"><img src="{{ asset('images/oprec/logo/ORTA.png') }}" alt="" class="image-fluid" onclick="mBlur();"></a>
-        </div>
-        <div class="logo-submit">
-            <a href="#3" class="open-popup"><img src="{{ asset('images/oprec/logo/LUA.png') }}" alt="" class="image-fluid" onclick="mBlur();"></a>
-        </div> -->
+        <footer>
     </div>
-</div>
+</div>    
+
 @endsection
 
 @section('custom-js')
