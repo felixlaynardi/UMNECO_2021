5aa734cc2e5bc4a3a2b130ba67880ee7336b2712
chotiwood <chotiwut11@gmail.com>
2021-04-07 01:30:10 +0700

    Update regis

--- resources/views/cms/page/register.blade.php
+++ resources/views/cms/page/register.blade.php
@@ -21,27 +21,27 @@
             <form  action="{{route('register')}}" method="post" class="form-">
                 @csrf
                 <label for="Firstname">Nama Depan</label>
-                <input type="text" name="Firstname" id="Firstname" placeholder="Umn" value="{{ old('Firstname') }}" class="form-control" required>
+                <input type="text" name="Firstname" id="Firstname" placeholder="Umn" value="{{ old('Firstname') }}" class="form-control mb-2" required>
                 @if($errors->has('Firstname'))
                 <div class=" error">{{ $errors->first('Firstname') }}</div>
                 @endif
                 <label for="Lastname">Nama Belakang</label>
-                <input type="text" name="Lastname" id="Lastname" placeholder="Eco" value="{{ old('Lastname') }}" class="form-control" required>
+                <input type="text" name="Lastname" id="Lastname" placeholder="Eco" value="{{ old('Lastname') }}" class="form-control mb-2" >
                 @if($errors->has('Lastname'))
                 <div class=" error">{{ $errors->first('Lastname') }}</div>
                 @endif
                 <label for="Student_id">NIM</label>
-                <input type="text" name="Student_id" id="Student_id" placeholder="0000001234" value="{{ old('Student_id') }}" class="form-control" required>
+                <input type="text" name="Student_id" id="Student_id" placeholder="0000001234" value="{{ old('Student_id') }}" class="form-control mb-2" required>
                 @if($errors->has('Student_id'))
                 <div class="error">{{ $errors->first('Student_id') }}</div>
                 @endif
                 <label for="Email">Email</label>
-                <input type="text" name="Email" id="Email" placeholder="eco@umn.ac.id" value="{{ old('Email') }}" class="form-control" required>
+                <input type="text" name="Email" id="Email" placeholder="eco@umn.ac.id" value="{{ old('Email') }}" class="form-control mb-2" required>
                 @if($errors->has('Email'))
                 <div class="error">{{ $errors->first('Email') }}</div>
                 @endif
                 <label for="Major">Jurusan</label>
-                <select id="Major" name="Major" value="{{ old('Major') }}" class="form-control" required>
+                <select id="Major" name="Major" value="{{ old('Major') }}" class="form-control mb-2" required>
                     <option value="Informatika">Informatika</option>
                     <option value="Teknik Komputer">Teknik Komputer</option>
                     <option value="Teknik Elektro">Teknik Elektro</option>
@@ -63,7 +63,7 @@
                 <div class="error">{{ $errors->first('major') }}</div>
                 @endif
                 <label for="Generation">Angkatan</label>
-                <select id="Generation" name="Generation" value="{{ old('Generation') }}" class="form-control" required>
+                <select id="Generation" name="Generation" value="{{ old('Generation') }}" class="form-control mb-2" required>
                     <option value="2014">2014</option>
                     <option value="2015">2015</option>
                     <option value="2016">2016</option>
@@ -76,27 +76,27 @@
                 <div class="error">{{ $errors->first('Generation') }}</div>
                 @endif
                 <label for="Instagram_account">Username IG (Tidak di private) </label>
-                <input type="text" name="Instagram_account" id="Instagram_account" placeholder="umn_eco" value="{{ old('Instagram_account') }}" class="form-control" required>
+                <input type="text" name="Instagram_account" id="Instagram_account" placeholder="umn_eco" value="{{ old('Instagram_account') }}" class="form-control mb-2" required>
                 @if($errors->has('Instagram_account'))
                 <div class="error">{{ $errors->first('Instagram_account') }}</div>
                 @endif
                 <label for="Generation">ID Line</label>
-                <input type="text" name="Line_id" id="Line_id" placeholder="line_id" value="{{ old('Line_id') }}" class="form-control" required>
+                <input type="text" name="Line_id" id="Line_id" placeholder="line id" value="{{ old('Line_id') }}" class="form-control mb-2" required>
                 @if($errors->has('Line_id'))
                 <div class="error">{{ $errors->first('Line_id') }}</div>
                 @endif
                 <label for="Phone_number">Nomor Telepon</label>
-                <input type="text" name="Phone_number" id="Phone_number" placeholder="08123456789" value="{{ old('Phone_number') }}" class="form-control" required>
+                <input type="text" name="Phone_number" id="Phone_number" placeholder="08123456789" value="{{ old('Phone_number') }}" class="form-control mb-2" required>
                 @if($errors->has('Phone_number'))
                 <div class="error">{{ $errors->first('Phone_number') }}</div>
                 @endif
                 <label for="Password">Password</label>
-                <input type="password" name="Password" id="Password" placeholder="password" class="form-control" required>
+                <input type="password" name="Password" id="Password" placeholder="password" class="form-control mb-2" required>
                 @if($errors->has('Password'))
                 <div class="error">{{ $errors->first('Password') }}</div>
                 @endif
                 <label for="Password_Confirmation">Konfirmasi Password</label>
-                <input type="password" name="Password_confirmation" id="Password_confirmation" placeholder="Confirm Password" class="form-control" required><br>
+                <input type="password" name="Password_confirmation" id="Password_confirmation" placeholder="Confirm Password" class="form-control mb-2" required><br>
                 @if($errors->has('Password_confirmation'))
                 <div class="error">{{ $errors->first('Password_confirmation') }}</div>>
                 @endif
@@ -115,8 +115,8 @@
                 
 </div>
 <footer id="footer">
-    <img src="{{ asset('images/regis/Logo Footer.png')}}" class="logo">
-    <h6 class="foot">© UMN ECO</h6>
+    <img src="{{ asset('images/regis/Logo Footer.png')}}" class="logo mx-auto">
+    <h6 class="foot mx-auto">© UMN ECO</h6>
 </footer>
 </header>
 
@@ -133,36 +133,11 @@
     Swal.fire("Selamat datang", "Data yang di input tidak valid", "error");
 </script>
 @endif
-@endsection
 <script>
-// const steps = Array.form(document.querySelectorAll('form .step'));
-// const nextBtn = document.getElementByID('#next-btn');
-// nextBtn.on("click",funtion(){console.log("test")})
+$( "#next-btn" ).click(function() {
+        $('.step-1').removeClass('active');
+        $('.step-2').addClass('active');
+    });
 
-$("#next-btn").click(function(){
-  console.log("The paragraph was clicked.");
-});
-
-// nextBtn.foreach(button=>{
-//     button.addEventListener('click', () => {
-//         changeStep('next');
-//     })
-// })
-
-// form.addEventListener(
-
-// )
-
-// function changeStep(btn) {
-//     let index = 0;
-//     const active = document.querySelector('form .step.active');
-//     index = steps.indexOf(active);
-//     steps[index].classList.remove('active');
-//     if(btn === 'next'){
-//         index++;
-//     }
-//     steps[index].classList.add('active')
-//     console.log(index)
-    
-// }
 </script>
+@endsection
