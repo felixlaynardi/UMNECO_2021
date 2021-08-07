b97857b3f97505061efcf1e37aec1342eed72b27
Alexander Moya Hin <Alexander.hin@student.umn.ac.id>
2021-08-07 12:41:07 +0700

    Create Table Database
    Create Table for
    - Takeaway Master
    - Order
    - New Eco Friends

---
+++ database/migrations/2021_08_07_041938_green_order.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Order extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('green_order', function (Blueprint $table) {
+            $table->id();
+            $table->foreign('user_id')->references('id')->on('green_eco_friends');
+            $table->boolean('dine_in');
+            $table->foreign('takeaway_id')->references('id')->on('green_takeaway');
+            $table->boolean('ordered');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('green_order');
+    }
+}
