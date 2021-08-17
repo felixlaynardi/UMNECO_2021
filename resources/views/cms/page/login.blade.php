02cfd2d4f35ba28056a58c903816195729f3c2a4
kevkrisss <kevin.jonathan5@student.umn.ac.id>
2021-08-17 19:51:22 +0700

    Add Assets

--- resources/views/cms/page/login.blade.php
+++ resources/views/cms/page/login.blade.php
@@ -21,7 +21,7 @@
                     <form action="{{route('login')}}" id="form-login" method="post">
                         @csrf
                         <div class="form-group mb-2">
-                            <label>Email</label>
+                            <label>EMAIL</label>
                             <input type="text" name="email" id="email" placeholder="eco@umn.ac.id" class="form-control">
                             @if($errors->has('email'))
                             <div class="error">{{ $errors->first('email') }}</div>
@@ -29,15 +29,15 @@
                             <br>
                         </div>
                         <div class="form-group mb-2">
-                            <label>Password</label>
+                            <label>PASSWORD</label>
                             <input type="password" name="password" id="password" placeholder="*********" class="form-control">
                             @if($errors->has('password'))
                             <div class="error">{{ $errors->first('password') }}</div>
                             @endif
                             <br><br>
                         </div>
-                        <button class="button p-2" id="btn-login" type="submit" name="action" value="login">Login</button>
-                        <a class="button p-2 btn-register" type="submit" href="{{ route('registration' )}}" value="register">Register</a>
+                        <button class="button p-2" id="btn-login" type="submit" name="action" value="login">LOGIN</button>
+                        <a class="button p-2 btn-register" type="submit" href="{{ route('registration' )}}" value="register">REGISTER</a>
                     </form>
                 </div>
             </div>
