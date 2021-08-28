f64ce7fd7a5b034da16351e2540fabf9a5893859
chotiwood <chotiwut11@gmail.com>
2021-08-28 20:48:53 +0700

    carousel

--- resources/views/cms/page/greenate/order_dummy.blade.php
+++ resources/views/cms/page/greenate/order_dummy.blade.php
@@ -10,7 +10,6 @@
         <p>CHOOSE YOUR ORDER</p>
     </div>
 </header>
-
     <div class="container-fluid mx-auto">
         <div class="row">
         <img src="{{ asset('images/order/Ayam_1.png') }}" class="ayam">
@@ -19,7 +18,7 @@
     </div>
 
     <div class="col-md-10 col-10 mid">
-    <h5 class="shoppingcart"><button type="submit" href="" class="mx-auto">My Shopping Cart </button><p class="Notif" id="cartNumber">0</p></h5>
+    <h5 class="shoppingcart"><button type="submit" class="mx-auto">My Shopping Cart </button><p class="Notif" id="cartNumber">0</p></h5>    
         <div class="main">
             {{-- <form action="" method="post">
                 <ul class="Add">
@@ -35,12 +34,12 @@
                 <div class="col-md-1 col-1">
                 </div>
                 <div class="col-md-10 col-10 box1">
-                    <div class="dineIn">
+                    <div class="dineIn active" id="dineIn">
                         <img src="{{ asset('images/order/Dine-In.png') }}" class="DineInLogo">
                         <h3 class="judul">Dine-In</h3>
                         <p class="dinein">Lorem ipsum dolor sit amet,<br> consectetur adipiscing elit,<br> sed do eiusmod</p>
                         <input type="checkbox" id="dine-in" name="Dinein_choice" value="1" class="addToCartNumberCheckbox" hidden>
-                        <label for="dine-in" class="hidden">Add to cart</label><br><br><br>
+                        <label for="dine-in" onclick="change" class="add">Add to cart</label><br><br><br>
                     </div>
                 </div>
                 <div class="col-md-1 col-1">
@@ -49,27 +48,89 @@
             <div class="col-md-12 col-12 row greenate">
                 <img src="{{ asset('images/order/Greenate.png') }}">
             </div>
-            <div class="row">
+
+            <!------------------- Monitor --------------------------->
+
+            <div class="row monitor">
                 <div class="col-md-1 col-1">
                 </div>
-                <div class="col-md-10 col-10">
-                    <div class="row">
-                    <p></p>
-                    @foreach ($Takeaways_Result as $res)
-                        <div class="col-md-2 col-2 pilihan mx-auto">
-                        <h4 class="takeaway">{{ $res->name }} </h4>
-                        <p class="skkm">1 SKKM Pengabdian</p>
-                        <input type="radio" id="{{ $res->name }}" name="Takeaway_choice" value="{{ $res->id }}" class="addToCartNumberRadio" hidden>
-                        <label for="{{ $res->name }}" id="addToCartNumber" class="tombol">Add to cart</label><br>
-                        </div>
-                    @endforeach
-                    </div>
+                <div class="col-md-2 col-2 pilihan mx-auto">
+                <img src="{{ asset('images/order/Tidbit.png') }}" class="images">
+                <h3 class="takeaway">Tidbit</h3>
+                <p class="skkm">1 SKKM Pengabdian</p>
+                <input type="checkbox" id="tidbit" class="" hidden name="Tidbit" value="Tidbit" class="takeAwayOption">
+                <label for="Tidbit" class="tombol"> Add to Cart</label><br>
+                </div>
+                <div class="col-md-2 col-2 pilihan mx-auto">
+                <img src="{{ asset('images/order/Exhort.png') }}" class="images">    
+                <h3 class="takeaway">Exhort</h3>
+                <p class="skkm">1 SKKM Pengabdian</p>
+                <input type="checkbox" id="exhort" class="" hidden name="Exhort" value="Exhort" class="takeAwayOption">
+                <label for="Exhort" class="tombol">Add to Cart</label><br>
+                </div>
+                <div class="col-md-2 col-2 pilihan mx-auto">
+                <img src="{{ asset('images/order/Alter.png') }}" class="images">
+                <h3 class="takeaway">Alter</h3>
+                <p class="skkm">1 SKKM Pengabdian</p>
+                <input type="checkbox" id="alter" class="" hidden name="Alter" value="Alter" class="takeAwayOption">
+                <label for="Alter" class="tombol">Add to Cart</label><br>
                 </div>
                 </form>
                 <div class="col-md-1 col-1">
                 </div>
             </div>
-            </div>
+
+            <!-- ------------------ Small device ------------------------ -->
+            <div class="col-12 small-device row">
+                <div class="col-md-10 bg">
+                <div id="carouselExampleControls" class="carousel slide d-lg-none d-block" data-bs-ride="carousel">
+                        <div class="carousel-inner row">
+                            <div class="item row carousel-item pilihan active box" data-bs-interval="1000000000">
+                                <div class="col-lg-5 col-sm-5 slide-logo">
+                                    <img src="{{ asset('images/order/Tidbit.png') }}" class="logo w-100 pb-5" >
+                                </div>
+                                <div class="col-lg-5 col-sm-12 info">
+                                <h3 class="title">Tidbit</h3>
+                                <p class="skkm">1 SKKM Pengabdian</p>
+                                <input type="checkbox" id="tidbit" class="tombol" hidden name="Tidbit" value="Tidbit" class="takeAwayOption">
+                                <label for="Tidbit" class="tombol add">Add to Cart</label><br>
+                                </div>
+                            </div>
+                            <div class="item row carousel-item pilihan  box" data-bs-interval="1000000000">
+                                <div class="col-lg-5 col-sm-5 slide-logo">
+                                    <img src="{{ asset('images/order/Exhort.png') }}" class="logo w-100 pb-5">
+                                </div>
+                                <div class="col-lg-5 col-sm-12 info">
+                                    <h3 class="title">Exhort</h3>
+                                    <p class="skkm">1 SKKM Pengabdian</p>
+                                    <input type="checkbox" id="exhort" class="" hidden name="Exhort" value="Exhort" class="takeAwayOption">
+                                    <label for="Exhort" class="tombol add">Add to Cart</label><br>
+                                </div>
+                            </div>
+                            <div class="item row carousel-item pilihan box" data-bs-interval="1000000000">
+                                <div class="col-lg-5 col-sm-5 slide-logo">
+                                    <img src="{{ asset('images/order/Alter.png') }}" class="logo w-100 pb-5">
+                                </div>
+                                <div class="col-lg-5 col-sm-12 info">
+                                    <h3 class="title">Alter</h3>
+                                    <p class="skkm">1 SKKM Pengabdian</p>
+                                    <input type="checkbox" id="alter" class="" hidden name="Alter" value="Alter" class="takeAwayOption">
+                                    <label for="Alter" class="tombol add">Add to Cart</label><br>
+                                </div>
+                            </div>
+                        </div>
+                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Previous</span>
+                        </button>
+                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Next</span>
+                        </button>
+                    </div>
+                </div>
+         
+
     </div>
     <div class="col-md-1 col-1">
     <img src="{{ asset('images/order/Burger.png') }}" class="burger">
@@ -80,4 +141,8 @@
 
 @section('custom-js')
 <script src="{{ asset('js/cms/page/order-page.js') }}"></script>
+
+<script>
+
+</script>
 @endsection
