5e971ddfb91046b9ad3b513fa9cb59f0130ffd96
chotiwood <chotiwut11@gmail.com>
2021-08-29 02:37:40 +0700

    shoppingcart carousel

--- resources/views/cms/page/greenate/order_dummy.blade.php
+++ resources/views/cms/page/greenate/order_dummy.blade.php
@@ -11,8 +11,8 @@
     </div>
 </header>
     <div class="container-fluid mx-auto">
-        <div class="row">
-        <img src="{{ asset('images/order/Ayam_1.png') }}" class="ayam">
+    <div class="row">
+    <img src="{{ asset('images/order/Ayam_1.png') }}" class="ayam">
         
     <div class="col-md-1 col-1">
     </div>
@@ -30,19 +30,15 @@
 
             <form action="{{ route('dummyShoppingCart') }}" method="post">
             @csrf
-            <div class="row">
-                <div class="col-md-1 col-1">
-                </div>
+            <div class="row justify-content-center" >
                 <div class="col-md-10 col-10 box1">
                     <div class="dineIn active" id="dineIn">
                         <img src="{{ asset('images/order/Dine-In.png') }}" class="DineInLogo">
                         <h3 class="judul">Dine-In</h3>
                         <p class="dinein">Lorem ipsum dolor sit amet,<br> consectetur adipiscing elit,<br> sed do eiusmod</p>
                         <input type="checkbox" id="dine-in" name="Dinein_choice" value="1" class="addToCartNumberCheckbox" hidden>
-                        <label for="dine-in" onclick="change" class="add">Add to cart</label><br><br><br>
+                        <label for="dine-in" onclick="change()" id="add" class="add">Add to cart</label><br><br><br>
                     </div>
-                </div>
-                <div class="col-md-1 col-1">
                 </div>
             </div>
             <div class="col-md-12 col-12 row greenate">
@@ -51,71 +47,67 @@
 
             <!------------------- Monitor --------------------------->
 
-            <div class="row monitor">
-                <div class="col-md-1 col-1">
+            <div class="col-md-10 col-10 row monitor justify-content-center">
+                <div class="col-md-2 col-2 pilihan mx-auto" id="Tidbit">
+                    <img src="{{ asset('images/order/Tidbit.png') }}" class="images">
+                    <h3 class="takeaway">Tidbit</h3>
+                    <p class="skkm" id="infoTidbit">1 SKKM Pengabdian</p>
+                    <input type="checkbox" id="tidbit" class="" hidden name="Tidbit" value="Tidbit" class="takeAwayOption">
+                    <label for="Tidbit" class="tombol" id="addTidbit" onclick="one()"> Add to Cart</label><br>
                 </div>
-                <div class="col-md-2 col-2 pilihan mx-auto">
-                <img src="{{ asset('images/order/Tidbit.png') }}" class="images">
-                <h3 class="takeaway">Tidbit</h3>
-                <p class="skkm">1 SKKM Pengabdian</p>
-                <input type="checkbox" id="tidbit" class="" hidden name="Tidbit" value="Tidbit" class="takeAwayOption">
-                <label for="Tidbit" class="tombol"> Add to Cart</label><br>
+                <div class="col-md-2 col-2 pilihan mx-auto" id="Exhort">
+                    <img src="{{ asset('images/order/Exhort.png') }}" class="images">    
+                    <h3 class="takeaway">Exhort</h3>
+                    <p class="skkm" id="infoExhort">1 SKKM Pengabdian</p>
+                    <input type="checkbox" id="exhort" class="" hidden name="Exhort" value="Exhort" class="takeAwayOption">
+                    <label for="Exhort" class="tombol" onclick="two()" id="addExhort">Add to Cart</label><br>
                 </div>
-                <div class="col-md-2 col-2 pilihan mx-auto">
-                <img src="{{ asset('images/order/Exhort.png') }}" class="images">    
-                <h3 class="takeaway">Exhort</h3>
-                <p class="skkm">1 SKKM Pengabdian</p>
-                <input type="checkbox" id="exhort" class="" hidden name="Exhort" value="Exhort" class="takeAwayOption">
-                <label for="Exhort" class="tombol">Add to Cart</label><br>
-                </div>
-                <div class="col-md-2 col-2 pilihan mx-auto">
-                <img src="{{ asset('images/order/Alter.png') }}" class="images">
-                <h3 class="takeaway">Alter</h3>
-                <p class="skkm">1 SKKM Pengabdian</p>
-                <input type="checkbox" id="alter" class="" hidden name="Alter" value="Alter" class="takeAwayOption">
-                <label for="Alter" class="tombol">Add to Cart</label><br>
+                <div class="col-md-2 col-2 pilihan mx-auto" id="Alter">
+                    <img src="{{ asset('images/order/Alter.png') }}" class="images">
+                    <h3 class="takeaway">Alter</h3>
+                    <p class="skkm" id="infoAlter">1 SKKM Pengabdian</p>
+                    <input type="checkbox" id="alter" class="" hidden name="Alter" value="Alter" class="takeAwayOption">
+                    <label for="Alter" class="tombol" onclick="three()" id="addAlter">Add to Cart</label><br>
                 </div>
                 </form>
-                <div class="col-md-1 col-1">
-                </div>
             </div>
 
             <!-- ------------------ Small device ------------------------ -->
-            <div class="col-12 small-device row">
-                <div class="col-md-10 bg">
+            <div class="col-12 small-device row justify-content-center">
+                <div class="col-12">
                 <div id="carouselExampleControls" class="carousel slide d-lg-none d-block" data-bs-ride="carousel">
                         <div class="carousel-inner row">
-                            <div class="item row carousel-item pilihan active box" data-bs-interval="1000000000">
-                                <div class="col-lg-5 col-sm-5 slide-logo">
+                            <div class="item carousel-item pilihan active box" id="TidbitSmall" data-bs-interval="1000000000">
+                                <div class="col-lg-10 col-sm-10 slide-logo">
                                     <img src="{{ asset('images/order/Tidbit.png') }}" class="logo w-100 pb-5" >
                                 </div>
-                                <div class="col-lg-5 col-sm-12 info">
+                                <div class="col-lg-10 col-sm-10 info">
                                 <h3 class="title">Tidbit</h3>
-                                <p class="skkm">1 SKKM Pengabdian</p>
+                                <p class="skkm" id="infoTidbitSm">1 SKKM Pengabdian</p>
                                 <input type="checkbox" id="tidbit" class="tombol" hidden name="Tidbit" value="Tidbit" class="takeAwayOption">
-                                <label for="Tidbit" class="tombol add">Add to Cart</label><br>
+                                <label for="Tidbit" class="tombol " onclick="one()" id="addTidbitSmall">Add to Cart</label><br>
                                 </div>
                             </div>
-                            <div class="item row carousel-item pilihan  box" data-bs-interval="1000000000">
-                                <div class="col-lg-5 col-sm-5 slide-logo">
+                            <div class="item carousel-item pilihan  box" data-bs-interval="1000000000" id="ExhortSmall">
+                                <div class="col-lg-10 col-sm-10 slide-logo">
                                     <img src="{{ asset('images/order/Exhort.png') }}" class="logo w-100 pb-5">
                                 </div>
-                                <div class="col-lg-5 col-sm-12 info">
+                                <div class="col-lg-10 col-sm-10 info">
                                     <h3 class="title">Exhort</h3>
-                                    <p class="skkm">1 SKKM Pengabdian</p>
+                                    <p class="skkm" id="infoExhortSm">1 SKKM Pengabdian</p>
                                     <input type="checkbox" id="exhort" class="" hidden name="Exhort" value="Exhort" class="takeAwayOption">
-                                    <label for="Exhort" class="tombol add">Add to Cart</label><br>
+                                    <label for="Exhort" class="tombol " onclick="two()" id="addExhortSmall">Add to Cart</label><br>
                                 </div>
                             </div>
-                            <div class="item row carousel-item pilihan box" data-bs-interval="1000000000">
-                                <div class="col-lg-5 col-sm-5 slide-logo">
+                            <div class="item carousel-item pilihan box" data-bs-interval="1000000000" id="AlterSmall">
+                                <div class="col-lg-10 col-sm-10 slide-logo">
                                     <img src="{{ asset('images/order/Alter.png') }}" class="logo w-100 pb-5">
                                 </div>
-                                <div class="col-lg-5 col-sm-12 info">
+                                <div class="col-lg-10 col-sm-10 info">
                                     <h3 class="title">Alter</h3>
-                                    <p class="skkm">1 SKKM Pengabdian</p>
+                                    <p class="skkm" id="infoAlterSm">1 SKKM Pengabdian</p>
                                     <input type="checkbox" id="alter" class="" hidden name="Alter" value="Alter" class="takeAwayOption">
-                                    <label for="Alter" class="tombol add">Add to Cart</label><br>
+                                    <label for="Alter" class="tombol " onclick="three()" id="addAlterSmall">Add to Cart</label><br>
                                 </div>
                             </div>
                         </div>
@@ -132,10 +124,10 @@
          
 
     </div>
-    <div class="col-md-1 col-1">
+    <div class="col-md-1 col-1 kanan">
     <img src="{{ asset('images/order/Burger.png') }}" class="burger">
-    </div>
     <img src="{{ asset('images/order/Kentang.png') }}" class="kentang">
+    </div>
     </div>
 @endsection
 
@@ -143,6 +135,69 @@
 <script src="{{ asset('js/cms/page/order-page.js') }}"></script>
 
 <script>
+function change() {
+    var element = document.getElementById("dineIn");
+    element.classList.toggle("style");
 
+    var element = document.getElementById("add");
+    element.classList.toggle("bttn");
+}
+function one() {
+    var element = document.getElementById("Tidbit");
+    element.classList.toggle("styleTakeAway");
+
+    var element = document.getElementById("infoTidbit");
+    element.classList.toggle("information");
+
+    var element = document.getElementById("addTidbit");
+    element.classList.toggle("bttn");
+
+    var element = document.getElementById("TidbitSmall");
+    element.classList.toggle("styleTakeAway");
+
+    var element = document.getElementById("infoTidbitSm");
+    element.classList.toggle("information");
+
+    var element = document.getElementById("addTidbitSmall");
+    element.classList.toggle("bttn");
+    }
+function two() {
+    var element = document.getElementById("Exhort");
+    element.classList.toggle("styleTakeAway");
+
+    var element = document.getElementById("infoExhort");
+    element.classList.toggle("information");
+
+    var element = document.getElementById("addExhort");
+    element.classList.toggle("bttn");
+
+    var element = document.getElementById("ExhortSmall");
+    element.classList.toggle("styleTakeAway");
+
+    var element = document.getElementById("infoExhortSm");
+    element.classList.toggle("information");
+
+    var element = document.getElementById("addExhortSmall");
+    element.classList.toggle("bttn");
+}
+function three() {
+    var element = document.getElementById("Alter");
+    element.classList.toggle("styleTakeAway");
+
+    var element = document.getElementById("infoAlter");
+    element.classList.toggle("information");
+
+    var element = document.getElementById("addAlter");
+    element.classList.toggle("bttn");
+
+    var element = document.getElementById("AlterSmall");
+    element.classList.toggle("styleTakeAway");
+
+    var element = document.getElementById("infoAlterSm");
+    element.classList.toggle("information");
+
+    var element = document.getElementById("addAlterSmall");
+    element.classList.toggle("bttn");
+}
 </script>
 @endsection
