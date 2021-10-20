d9c9f50a15200436c749f206395d955c1a97caff
Alexander Moya Hin <Alexander.hin@student.umn.ac.id>
2021-10-20 22:12:25 +0700

    [Database] Creata Database
    Create Blue_eco_friends table and update EcoFriend Model

---
+++ database/migrations/2021_10_10_131417_blue_eco_friends.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class BlueEcoFriends extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blue_eco_friends', function (Blueprint $table) {
+            $table->id();
+            $table->string('full_name');
+            $table->string('email')->unique();
+            $table->string('student_id')->unique()->nullable();
+            $table->string('faculty')->nullable();
+            $table->string('major')->nullable();
+            $table->string('generation')->nullable();
+            $table->string('instagram_account')->unique();
+            $table->string('line_id')->unique();
+            $table->string('phone_number')->unique();
+            $table->string('password');
+            $table->timestamp('registration_time');
+            $table->string('task_link');
+            $table->timestamp('submission_time');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blue_eco_friends');
+    }
+}
