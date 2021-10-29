072fa37af1fb51cbbd00aa7b71a90e59346aef6c
lifosmin <65651883+lifosmin@users.noreply.github.com>
2021-10-29 21:32:49 +0700

    [Home Blue] Create homepage

--- routes/web.php
+++ routes/web.php
@@ -39,6 +39,10 @@ Route::get('/about-greenate', function () {
   return view('cms.page.about-greenate', ['title' => 'UMN ECO 2021 - About GREENATE']);
 })->name('about-greenate');
 
+Route::get('/home', function () {
+  return view('cms.page.home-blue', ['title' => 'UMN ECO 2021 - Home BLUE']);
+})->name('home-blue');
+
 // Route::post('/registration', [RegistrationController::class, 'index']);
 Route::get('/our-team', [OprecController::class, 'index'])->name('ourteam');
 // Route::post('/open-recruitment', [OprecController::class, 'store'])->name('oprecPost');
