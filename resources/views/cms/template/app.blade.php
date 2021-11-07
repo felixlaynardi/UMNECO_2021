e6108d8db29d939de43e80fe955af5922542f660
Felix Laynardi <felixxlay@gmail.com>
2021-11-07 20:17:58 +0700

    Update loading page

--- resources/views/cms/template/app.blade.php
+++ resources/views/cms/template/app.blade.php
@@ -34,7 +34,7 @@
 <body>
     <!-- Preloader -->
     <div id="preloader">
-        <lottie-player src="{{ asset('images/loading2.json') }}"   speed="1"  loop autoplay style="position:fixed;width:40%;top:50%;left:50%;transform: translate(-50%, -50%);text-align:center;"></lottie-player>
+        <lottie-player src="{{ asset('images/loading3.json') }}"   speed="1"  loop autoplay class="lottie-gif col-lg-2 col-sm-4 col-6"></lottie-player>
         <!-- <img src="{{ asset('images/gif-logo.gif') }}" class="loading-img" > -->
     </div>
 
